996766be314b8bacbc53915d2203ed59fcdbcc87
Davi Guarda <davi.guarda.b@hotmail.com>
2024-03-31 11:47:02 -0300

    Revert "test: Adjusted packages"
    This reverts commit 0dfbb3748032999085f5d39f0a93d7f0948fcf01.

--- src/main/java/br/com/davidev/attusdesafiobackend/service/implementation/AddressServiceImpl.java
+++ src/main/java/br/com/davidev/attusdesafiobackend/service/implementation/AddressServiceImpl.java
@@ -7,6 +7,7 @@ import br.com.davidev.attusdesafiobackend.repository.AddressRepository;
 import br.com.davidev.attusdesafiobackend.repository.PersonRepository;
 import br.com.davidev.attusdesafiobackend.service.AddressService;
 import br.com.davidev.attusdesafiobackend.util.ExceptionUtils;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -68,6 +69,7 @@ public class AddressServiceImpl implements AddressService {
     }
 
     @Override
+    @Transactional
     public void setDefaultAddress(Long idPerson, Long idAddress) {
 
         var address = addressRepository.findById(idAddress)
